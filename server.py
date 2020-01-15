dc2a89cbf37c326b7850129affc51c0a4886c774
wguo6358 <wguo6358@gmail.com>
2020-01-14 18:03:12 -0600

    Finish server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from colorama import Fore,Back,Style
 import datetime
-import http.server
+import web
 
 print("This version is a beta version, if you find some bugs, please report to us.")
 print(Fore.YELLOW+"Warning: http.server is not recommended for production. It only implements basic security checks.")
@@ -16,19 +16,28 @@ else:
     qqids=["1","2","3","4","5"]
 
 total=len(qqids)
-index=0
-
+indexnum=-1
 print("Finished loading files, please start your Android device/emulator and connect.")
 
-for i in qqids:
-    print("Waiting for next add")
-    print(Fore.MAGENTA+"["+str(datetime.datetime.now())+"]\n"+"Adding "+i+Fore.RESET)
-    try:
-        #code start here
 
-        #end of main code
-        index+=1
-        print(str(index)+" added, "+str(total)+" left")
-    except:
-        print("Error during adding "+i)
-    print()
+class index:
+    def GET(self):
+        global indexnum
+        try:
+            if indexnum>=total:
+                print("Finish!")
+                input()
+            print(str(indexnum)+" added, "+str(total)+" total.")
+            print(Fore.MAGENTA+"["+str(datetime.datetime.now())+"]\n"+"Adding "+str(qqids[indexnum])+Fore.RESET)
+            indexnum+=1
+            print()
+            return qqids[indexnum]
+        except:
+            print(Fore.RED+"Error during adding "+qqids[indexnum]+Fore.RESET)
+        
+
+if __name__ == "__main__":
+    app = web.application(("/","index"), globals())
+    app.run()
+
+
